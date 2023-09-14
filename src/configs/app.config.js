7992ce9e7170205b17bef3a1f64af456d6b01f2f
Siddharth Rawat <rawat.sid@northeastern.edu>
2023-09-14 10:02:19 -0400

    feat: simple API with healthcheck endpoint

---
+++ src/configs/app.config.js
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+
+const { HOSTNAME, PORT } = process.env
+
+const appConfig = {
+  HOSTNAME,
+  PORT,
+}
+
+export default appConfig
---
+++ src/routes/health.route.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { health } from '../controllers/health.controller'
+
+const router = express.Router()
+
+router.get('/', health)
+router.get('/health', health)
+
+export {router as healthRoute }
